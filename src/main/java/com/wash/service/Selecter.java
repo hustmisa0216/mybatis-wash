775e49fd40cff0c8f8382a80e779b6b0d1b1bbbc
hustmisa0216 <liukunpeng90@163.com>
2025-04-15 21:42:23 +0800

    add

--- src/main/java/com/wash/service/Selecter.java
+++ src/main/java/com/wash/service/Selecter.java
@@ -350,8 +350,6 @@ public class Selecter {
         double sum = stats.getSum();
 
         DecData decData = venCalculator.calculateAmount(inputVendorId,sum, inputDecAmount);
-
-
         if(decData==null){
             return null;
         }
